ff9809a8948d45d7774c3246688947f6600bd73b
홍성민 <seanhong2000@gmail.com>
2023-01-05 14:08:18 +0900

    Initial Commit

---
+++ MemoCard/MemoCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoCardApp.swift
+//  MemoCard
+//
+//  Created by Sean Hong on 2023/01/05.
+//
+
+import SwiftUI
+
+@main
+struct MemoCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
